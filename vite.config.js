154be77676e7323742c38316a2c33849bc5e7b16
dzz <1464157533@qq.com>
2022-09-27 15:41:34 +0800

    更新配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: DZZ
  * @Date: 2022-09-27 13:57:16
- * @LastEditTime: 2022-09-27 15:34:53
+ * @LastEditTime: 2022-09-27 15:41:03
  * @LastEditors: DZZ
  * @Description: 
  */
@@ -12,7 +12,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: import.meta.env.NODE_ENV === 'production' ? '/circle-demo' : '/',
+  base: './',
   plugins: [ vue() ],
   resolve: {
     alias: {
