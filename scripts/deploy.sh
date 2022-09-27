97686ce32b78551c8838f8da402f3b278be9fe2a
dzz <1464157533@qq.com>
2022-09-27 15:23:16 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,43 +1,47 @@
-#!bin/bash
-###
- # @Author: DZZ
- # @Date: 2022-09-27 14:27:21
- # @LastEditTime: 2022-09-27 14:35:49
- # @LastEditors: DZZ
- # @Description: 
-### 
+#!/bin/sh
 
 set -e
 
+# 打印当前的工作路径
 pwd
 remote=$(git config remote.origin.url)
 
 echo 'remote is: '$remote
 
+# 新建一个发布的目录
 mkdir gh-pages-branch
 cd gh-pages-branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+# 创建的一个新的仓库
+# 设置发布的用户名与邮箱
+git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
+git config --global user.name "$GH_NAME" >/dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
+
 echo 'email is: '$GH_EMAIL
 echo 'name is: '$GH_NAME
-echo 'sitesource is :' $siteScource
+echo 'sitesource is: '$siteSource
 
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-then
+# 切换gh-pages分支
+if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
     git checkout gh-pages
+    # 删掉旧的内容
     git rm -rf .
-else
+else 
     git checkout --orphan gh-pages
 fi
 
-cp -a "../${siteScource}/." .
+# 把构建好的文件目录给拷贝进来
+cp -a "../${siteSource}/." .
+
 ls -la
 
+# 把所有的文件添加到git
 git add -A
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
-git push --force --quiet origin ph-pages
+git push --force --quiet origin gh-pages
+
+# 资源回收，删除临时分支与目录
 cd ..
 rm -rf gh-pages-branch
 
