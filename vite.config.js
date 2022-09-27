9c7b3f9b39e037457483a468a0f2b14358e966e6
dzz <1464157533@qq.com>
2022-09-27 15:02:45 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+/*
+ * @Author: DZZ
+ * @Date: 2022-09-27 13:57:16
+ * @LastEditTime: 2022-09-27 15:01:50
+ * @LastEditors: DZZ
+ * @Description: 
+ */
+import { fileURLToPath,URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  publicPath: "./",
+  plugins: [ vue() ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',import.meta.url))
     }
   }
 })
